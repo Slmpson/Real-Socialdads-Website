40c1a5e690356d82a3041e20b7962e097ac6e8b5
Craig Simpson <simpson_cmes@hotmail.co.uk>
2021-12-24 13:43:11 +0000

    Home page updated. Stats tables added

--- script.js
+++ script.js
@@ -5,3 +5,56 @@ const lastModified = () => {
 
 lastModified();
 
+
+
+
+if (typeof gsap !== undefined) {
+    if (gsap) {
+        let timelineSectionOne = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.tactics-section-one',
+                start: 'top',
+                end: '100%',
+                scrub: 'true',
+                pin: true,
+            }
+        });
+
+        let timelineSectionTwo = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.tactics-section-two',
+                start: 'top',
+                end: '100%',
+                scrub: 'true',
+                pin: true,
+            }
+        })
+
+        let timelineSectionThree = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.tactics-section-three',
+                start: 'top',
+                end: '100%',
+                scrub: 'true',
+                pin: true,
+            }
+        })
+
+
+        timelineSectionOne.fromTo('.tactics-section-one__title', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionOne.fromTo('.tactics-section__subtitle', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionTwo.fromTo('.tactics-section-two__title', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionTwo.fromTo('.quote-section-two', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionTwo.fromTo('.quote-citation-section-two', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionThree.fromTo('.tactics-section-three__title', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionThree.fromTo('.quote-section-three', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionThree.fromTo('.quote-citation-section-three', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionThree.fromTo('.section-three-image', { opacity: 0 }, { opacity: 1, duration: 1 });
+    }
+}
+
+
+
+
+
+
