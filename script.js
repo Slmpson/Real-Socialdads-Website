184799f8c1110380d903b680bd1487e58e0f50d7
Craig Simpson <simpson_cmes@hotmail.co.uk>
2022-01-03 14:52:31 +0000

    canvas draw element added. Works on scroll and resize

--- script.js
+++ script.js
@@ -1,66 +1,78 @@
-// const lastModified = () => {
-//     const modified = document.lastModified;
-//     document.getElementById('lastModified').innerHTML = `Last updated ${modified}`
-// }
+const lastModified = () => {
+    const modified = document.lastModified;
+    document.getElementById('lastModified').innerHTML = `Last updated ${modified}`
+}
 
-// lastModified();
-
-
+lastModified();
 
 
-// if (typeof gsap !== undefined) {
-//     if (gsap) {
-//         let timelineSectionOne = gsap.timeline({
-//             scrollTrigger: {
-//                 trigger: '.tactics-section-one',
-//                 start: 'top',
-//                 end: '100%',
-//                 scrub: 'true',
-//                 pin: true,
-//             }
-//         });
-
-//         let timelineSectionTwo = gsap.timeline({
-//             scrollTrigger: {
-//                 trigger: '.tactics-section-two',
-//                 start: 'top',
-//                 end: '100%',
-//                 scrub: 'true',
-//                 pin: true,
-//             }
-//         })
-
-//         let timelineSectionThree = gsap.timeline({
-//             scrollTrigger: {
-//                 trigger: '.tactics-section-three',
-//                 start: 'top',
-//                 end: '100%',
-//                 scrub: 'true',
-//                 pin: true,
-//             }
-//         })
 
 
-//         timelineSectionOne.fromTo('.tactics-section-one__title', { opacity: 0 }, { opacity: 1, duration: 1 });
-//         timelineSectionOne.fromTo('.tactics-section__subtitle', { opacity: 0 }, { opacity: 1, duration: 1 });
-//         timelineSectionTwo.fromTo('.tactics-section-two__title', { opacity: 0 }, { opacity: 1, duration: 1 });
-//         timelineSectionTwo.fromTo('.quote-section-two', { opacity: 0 }, { opacity: 1, duration: 1 });
-//         timelineSectionTwo.fromTo('.quote-citation-section-two', { opacity: 0 }, { opacity: 1, duration: 1 });
-//         timelineSectionThree.fromTo('.tactics-section-three__title', { opacity: 0 }, { opacity: 1, duration: 1 });
-//         timelineSectionThree.fromTo('.quote-section-three', { opacity: 0 }, { opacity: 1, duration: 1 });
-//         timelineSectionThree.fromTo('.quote-citation-section-three', { opacity: 0 }, { opacity: 1, duration: 1 });
-//         timelineSectionThree.fromTo('.section-three-image', { opacity: 0 }, { opacity: 1, duration: 1 });
-//     }
-// }
+if (typeof gsap !== undefined) {
+    if (gsap) {
+        let timelineSectionOne = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.tactics-section-one',
+                start: 'top',
+                end: '100%',
+                scrub: 'true',
+                pin: true,
+            }
+        });
+
+        let timelineSectionTwo = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.tactics-section-two',
+                start: 'top',
+                end: '100%',
+                scrub: 'true',
+                pin: true,
+            }
+        })
+
+        let timelineSectionThree = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.tactics-section-three',
+                start: 'top',
+                end: '100%',
+                scrub: 'true',
+                pin: true,
+            }
+        })
+
+
+        timelineSectionOne.fromTo('.tactics-section-one__title', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionOne.fromTo('.tactics-section__subtitle', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionTwo.fromTo('.tactics-section-two__title', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionTwo.fromTo('.quote-section-two', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionTwo.fromTo('.quote-citation-section-two', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionThree.fromTo('.tactics-section-three__title', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionThree.fromTo('.quote-section-three', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionThree.fromTo('.quote-citation-section-three', { opacity: 0 }, { opacity: 1, duration: 1 });
+        timelineSectionThree.fromTo('.section-three-image', { opacity: 0 }, { opacity: 1, duration: 1 });
+    }
+}
 
 window.addEventListener("load", () => {
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
+    let offsetX;
+    let offsetY;
 
     //resizing
     // could add function window.addEventListener('resize' (run function)) to re-adjust canvas size
     // canvas.height = window.innerHeight;
     // canvas.width = window.innerWidth;
+
+
+    const reOffset = () => {
+        let BB = canvas.getBoundingClientRect();
+        offsetX = BB.left;
+        offsetY = BB.top;
+    }
+
+    window.onscroll=function(e){ reOffset(); }
+    window.onresize=function(e){ reOffset(); }
 
     // // order matters, top has an effect on that below
     ctx.strokeStyle = 'red'
@@ -79,34 +91,30 @@ window.addEventListener("load", () => {
     function startPosition(e) {
         painting = true;
         draw(e); // allows dots, dont have to drag mouse
-        console.log('start')
     }
 
     function finishedPosition() {
         painting = false;
         ctx.beginPath(); // resets being position so lines dont auto connect
-        console.log('finished')
     }
 
     function draw(e){
+        mouseX=parseInt(e.clientX-offsetX);
+        mouseY=parseInt(e.clientY-offsetY);
+
         if(!painting) return;
         ctx.lineWidth = 10;
         ctx.lineCap = 'round';
 
-        ctx.lineTo(e.clientX, e.clientY);
+        ctx.lineTo(mouseX, mouseY);
         ctx.stroke();
         ctx.beginPath();
-        ctx.moveTo(e.clientX, e.clientY)
-        console.log('drawing')
+        ctx.moveTo(mouseX, mouseY)
     }
     //event listeners
     canvas.addEventListener('mousedown', startPosition);
     canvas.addEventListener('mouseup', finishedPosition);
     canvas.addEventListener('mousemove', draw);
-    
-
-    
-    
 });
 
 
