a4990f81c352d40e2bf0b562b9ce514fe09e081c
Craig Simpson <simpson_cmes@hotmail.co.uk>
2022-02-23 14:14:23 +0000

    Added all chicken chat volumes

--- js/volumeNav.js
+++ js/volumeNav.js
@@ -8,11 +8,11 @@ const createVolumeNav = () => {
             <li><a href="chickenchat_vol2.html">Volume 2</a></li>
             <li><a href="chickenchat_vol3.html">Volume 3</a></li>
             <li><a href="chickenchat_vol4.html">Volume 4</a></li>
-            <li class="unavailable"><a href="chickenchat_vol5.html" class="unavailable">Volume 5</a></li>
-            <li class="unavailable"><a href="chickenchat_vol6.html" class="unavailable">Volume 6</a></li>
-            <li class="unavailable"><a href="chickenchat_vol7.html" class="unavailable">Volume 7</a></li>
-            <li class="unavailable"><a href="chickenchat_vol8.html" class="unavailable">Volume 8</a></li>
-            <li class="unavailable"><a href="chickenchat_vol9.html" class="unavailable">Volume 9</a></li>
+            <li><a href="chickenchat_vol5.html">Volume 5</a></li>
+            <li><a href="chickenchat_vol6.html">Volume 6</a></li>
+            <li><a href="chickenchat_vol7.html">Volume 7</a></li>
+            <li><a href="chickenchat_vol8.html">Volume 8</a></li>
+            <li><a href="chickenchat_vol9.html">Volume 9</a></li>
         </ul>
     </div>
     `
